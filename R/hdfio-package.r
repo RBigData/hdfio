49ba806dbc927ec2b2053a171997a5e63a3dfe23
wrathematics <wrathematics@gmail.com>
2018-05-08 14:18:42 -0400

    add hdfio-package.r

---
+++ R/hdfio-package.r
@@ -0,0 +1,10 @@
+#' hdfio
+#' 
+#' A set of high-level utilities for working with hdf5.
+#' 
+#' @importFrom hdf5r h5file
+#' 
+#' @name hdfio-package
+#' @docType package
+#' @keywords Package
+NULL
