d7b8e07b7c638b9facfad85ecce81e1c16449f07
wrathematics <wrathematics@gmail.com>
2018-06-25 22:09:51 -0400

    refactor is_h5df()

--- R/is_h5df.r
+++ R/is_h5df.r
@@ -1,39 +1,3 @@
-is_h5df_from_pandas = function(h5df_fp, dataset)
-{
-  !is.null(hdf5r::h5attributes(h5df_fp[[dataset]])$pandas_version)
-}
-
-
-
-check_is_h5df_from_pandas = function(h5df_fp, dataset)
-{
-  valid = is_h5df_from_pandas(h5df_fp, dataset)
-  if (!valid)
-    stop("h5in/dataset does not point to a valid dataset/h5df file")
-  
-  invisible(TRUE)
-}
-
-
-get_dataset = function(h5df_fp, dataset)
-{
-  if (is.null(dataset))
-  {
-    datasets = names(h5df_fp)
-    if (length(datasets) != 1)
-    {
-      hdf5r::h5close(h5df_fp)
-      stop("multiple datasets available")
-    }
-    else
-      dataset = datasets[1]
-  }
-  
-  dataset
-}
-
-
-
 #' is_h5df
 #' 
 #' Check if an HDF5 dataset is dataframe-like.
@@ -69,10 +33,10 @@ is_h5df = function(h5in, dataset=NULL)
   if (!is.null(dataset))
     check.is.string(dataset)
   
-  f = hdf5r::h5file(h5in, mode="r")
-  dataset = get_dataset(f, dataset)
-  check = is_h5df_from_pandas(f, dataset)
-  hdf5r::h5close(f)
+  f = h5file(h5in, mode="r")
+  dataset = h5_get_dataset(f, dataset)
+  fmt = h5_detect_format(f, dataset, verbose=FALSE)
+  h5close(f)
   
-  check
+  fmt != "unknown"
 }
