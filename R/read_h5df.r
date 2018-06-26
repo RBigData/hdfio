e315eb553da96880142da28279cdc46722822307
wrathematics <wrathematics@gmail.com>
2018-06-25 22:11:22 -0400

    refactor readers

--- R/read_h5df.r
+++ R/read_h5df.r
@@ -1,3 +1,60 @@
+# -----------------------------------------------------------------------------
+# pytables readers
+# -----------------------------------------------------------------------------
+
+# pandas.HDFStore.put(format="fixed")
+read_pytables_fixed = function(h5_fp, dataset, rows)
+{
+  columns = list.datasets(h5_fp[[dataset]])
+  columns = columns[grep("block", columns)]
+  
+  if (any(grepl("block2", columns)))
+    close_and_stop("file has string data written in 'fixed' format, which can not be portably read. Please re-write from pandas/pytables with format='table'")
+  
+  colnames = h5_fp[[dataset]][["axis0"]][]
+  rownames = h5_fp[[dataset]][["axis1"]][]
+  
+  df = vector(mode="list", length=n)
+  names(df) = colnames
+  
+  for (i in seq(2, length(columns), by=2))
+  {
+    item = columns[i]
+    
+    if (is.null(rows))
+      tmp = h5_fp[[dataset]][[item]][1, ]
+    else
+      tmp = h5_fp[[dataset]][[item]][1, rows]
+    
+    df[h5_fp[[dataset]][columns[i-1]]][1]
+  }
+  
+  data.table::setDF(df, rownames=rownames) 
+  
+  df
+}
+
+
+
+# pandas.HDFStore.put(format="table")
+read_pytables_table = function(h5_fp, dataset, rows)
+{
+  if (is.null(rows))
+    df = h5_fp[[dataset]][["table"]][]
+  else
+    df = h5_fp[[dataset]][["table"]][rows]
+  
+  df$index = NULL
+  
+  df
+}
+
+
+
+# -----------------------------------------------------------------------------
+# interface
+# -----------------------------------------------------------------------------
+
 #' read_h5df
 #' 
 #' TODO
@@ -18,24 +75,33 @@
 #' df
 #' 
 #' @seealso
-#' \code{\link{is_h5df}}
+#' \code{\link{write_h5df}}
 #' 
 #' @export
-read_h5df = function(h5in, dataset=NULL)
+read_h5df = function(h5in, dataset=NULL, rows=NULL, verbose=FALSE)
 {
   check.is.string(h5in)
+  check.is.flag(verbose)
   if (!is.null(dataset))
     check.is.string(dataset)
+  if (!is.null(rows))
+  {
+    if (length(rows) == 0 || any(rows < 1) || !all(is.inty(rows)))
+      stop("argument 'rows' must be a vector of positive integers")
+  }
   
-  f = hdf5r::h5file(h5in, mode="r")
-  dataset = get_dataset(f, dataset)
-  check_is_h5df_from_pandas(f, dataset)
+  h5_fp = h5file(h5in, mode="r")
+  dataset = h5_get_dataset(h5_fp, dataset)
+  fmt = h5_detect_format(h5_fp, dataset, verbose)
   
-  len = f[[dataset]][["table"]]$dims
-  df = f[[dataset]][["table"]][1:len]
-  df$index = NULL
+  if (fmt == "pytables_table")
+    df = read_pytables_table(h5_fp, dataset, rows)
+  else if (fmt == "pytables_fixed")
+    df = read_pytables_fixed(h5_fp, dataset, rows)
+  else
+    close_and_stop(h5_fp, "unknown format")
   
-  hdf5r::h5close(f)
+  h5close(h5_fp)
   
   df
 }
