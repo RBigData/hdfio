d4c82057bab1ea49a7c8d9e628fa1205a793d9d8
wrathematics <wrathematics@gmail.com>
2018-07-03 13:37:52 -0400

    add simple progress printer

--- R/utils.r
+++ R/utils.r
@@ -19,3 +19,44 @@ csv_reader = function(file, skip="__auto__", nrows=Inf, stringsAsFactors=FALSE, 
 {
   data.table::fread(file=file, skip=skip, nrows=nrows, stringsAsFactors=stringsAsFactors, verbose=FALSE, data.table=FALSE, ...)
 }
+
+
+
+ndigits = function(n)
+{
+  if (n==0)
+    1L
+  else
+    as.integer(log10(n) + 1.0)
+}
+
+
+
+progress_printer = function(i, n, verbose)
+{
+  if (!isTRUE(verbose))
+    return(invisible())
+  
+  
+  nd = ndigits(n)
+  
+  if (i == 0)
+  {
+    cat("Processing batch ")
+    cat(paste0(rep(" ", nd), collapse=""))
+  }
+  else
+  {
+    nd_i = ndigits(i)
+    for (j in 1:(nd_i + nd + 1L))
+      cat("\b")
+    
+    cat(i)
+  }
+  
+  cat("/")
+  cat(n)
+  
+  if (i == n)
+    cat("\n")
+}
