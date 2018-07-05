c52f570fdaeeb160cb231946675b0a4da1394782
wrathematics <wrathematics@gmail.com>
2018-07-05 10:51:10 -0400

    drop dead function from native routines export

--- src/hdfio_native.c
+++ src/hdfio_native.c
@@ -6,10 +6,8 @@
 #include <stdlib.h>
 
 extern SEXP R_get_max_str_len(SEXP x);
-extern SEXP R_space_pad_strings(SEXP x);
 
 static const R_CallMethodDef CallEntries[] = {
-  {"R_space_pad_strings", (DL_FUNC) &R_space_pad_strings, 1},
   {"R_get_max_str_len", (DL_FUNC) &R_get_max_str_len, 1},
   {NULL, NULL, 0}
 };
